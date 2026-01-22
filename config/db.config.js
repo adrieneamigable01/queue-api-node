734b48e18a5ac65851c4d2776a1f88490882f3cf
Adriene Amigable <adrienecarrreamigable01@gmail.com>
2026-01-22 16:46:45 +0800

    updated

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: process.env.DB_HOST || "148.222.53.10", // MySQL host
+  HOST: process.env.DB_HOST || "srv2050.hstgr.io", // MySQL host
   USER: process.env.DB_USER || "u859692781_queque",
   PASSWORD: process.env.DB_PASS || "Thequck!123",
   DB: process.env.DB_NAME || "u859692781_queque",
